9a42a5d845f89a286d8f67c47b920e2a6c9386ee
vaishnavi <vasanjay@deloitte.com>
2024-06-06 10:00:31 +0530

    Basics

---
+++ Api Handling/Api Handling/Api_HandlingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Api_HandlingApp.swift
+//  Api Handling
+//
+//  Created by Sanjay, Vaishnavi on 06/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Api_HandlingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
