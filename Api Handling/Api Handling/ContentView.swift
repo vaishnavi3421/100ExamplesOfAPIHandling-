56bcf0528d57c01f9d7fc381912222e33d291209
vaishnavi <vasanjay@deloitte.com>
2024-06-06 11:38:16 +0530

    Courses

--- Api Handling/Api Handling/ContentView.swift
+++ Api Handling/Api Handling/ContentView.swift
@@ -7,15 +7,64 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct URLImage: View {
+    let urlString : String
+    
+    @State var data : Data?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        if let data = data ,let uiimage = UIImage(data: data){
+            Image (uiImage: uiimage)
+                .resizable()
+                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                .frame(width: 130, height: 70)
+                .background(Color.gray)
         }
-        .padding()
+        else {
+            Image(systemName: "video")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 130, height: 70)
+                .background(Color.gray)
+                .onAppear {
+                fetchData()
+            }
+        }
+    }
+    private func fetchData() {
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) {
+            data, _, _ in
+            self.data = data
+        }
+        task.resume()
+    }
+}
+
+
+struct ContentView: View {
+    @StateObject var viewModel = ViewModel()
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(viewModel.courses, id: \.self) { course in
+                    
+                    HStack {
+                        URLImage(urlString: course.image,data:nil)
+                        Text(course.name)
+                            .bold()
+                    }
+                    .padding(3)
+                }
+            }
+            .navigationTitle("Courses")
+            .onAppear {
+                viewModel.fetch()
+            }
+            
+        }
     }
 }
 
